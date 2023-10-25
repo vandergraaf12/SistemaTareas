a62decbfda5d164474b4657992a8484690bb9fd9
HIMT <himt823@gmail.com>
2023-10-25 19:26:40 -0300

    Listado de Tareas

--- src/main/java/himt/tareas/controlador/IndexControlador.java
+++ src/main/java/himt/tareas/controlador/IndexControlador.java
@@ -1,15 +1,64 @@
 package himt.tareas.controlador;
 
+import himt.tareas.modelo.Tarea;
+import himt.tareas.servicio.TareaServicio;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.*;
 
 @Component
 public class IndexControlador implements Initializable {
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
+
+    @Autowired
+    private TareaServicio tareaServicio;
+
+    @FXML
+    private TableView<Tarea> tareaTabla;
+
+    @FXML
+    private TableColumn<Tarea, Integer> idTareaColumna;
+
+    @FXML
+    private TableColumn<Tarea, String> nombreTareaColumna;
+
+    @FXML
+    private TableColumn<Tarea, String> responsableTareaColumna;
+
+    @FXML
+    private TableColumn<Tarea, String> estatusTareaColumna;
+
+    private final ObservableList<Tarea> tareaList = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        tareaTabla.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        configurarColumnas();
+        listarTareas();
+    }
 
+    private void configurarColumnas(){
+        idTareaColumna.setCellValueFactory(new PropertyValueFactory<>("idTarea"));
+        nombreTareaColumna.setCellValueFactory(new PropertyValueFactory<>("nombreTarea"));
+        responsableTareaColumna.setCellValueFactory(new PropertyValueFactory<>("responsable"));
+        estatusTareaColumna.setCellValueFactory(new PropertyValueFactory<>("estatus"));
+    }
+
+    private void listarTareas(){
+        logger.info("Ejecutando listado de tareas...");
+        tareaList.clear();
+        tareaList.addAll(tareaServicio.listarTareas());
+        tareaTabla.setItems(tareaList);
     }
 }
