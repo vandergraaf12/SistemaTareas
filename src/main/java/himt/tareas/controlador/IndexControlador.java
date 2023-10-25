61baaf13cb8ca392ebc283e44c36769addbf9f09
HIMT <himt823@gmail.com>
2023-10-25 20:25:39 -0300

    Agregar Tarea

--- src/main/java/himt/tareas/controlador/IndexControlador.java
+++ src/main/java/himt/tareas/controlador/IndexControlador.java
@@ -41,6 +41,15 @@ public class IndexControlador implements Initializable {
 
     private final ObservableList<Tarea> tareaList = FXCollections.observableArrayList();
 
+    @FXML
+    private TextField nombreTareaTexto;
+
+    @FXML
+    private TextField responsableTexto;
+
+    @FXML
+    private TextField estatusTexto;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         tareaTabla.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
@@ -60,5 +69,43 @@ public class IndexControlador implements Initializable {
         tareaList.clear();
         tareaList.addAll(tareaServicio.listarTareas());
         tareaTabla.setItems(tareaList);
+    }
+
+    @FXML
+    private void agregarTarea(){
+        if(nombreTareaTexto.getText().isEmpty()){
+            mostrarMensaje("Error Validacion","Debe ingresar una tarea");
+            nombreTareaTexto.requestFocus();
+            return;
+        }
+        else{
+            var tarea = new Tarea();
+            recolectarDatosFormulario(tarea);
+            tareaServicio.guardarTarea(tarea);
+            mostrarMensaje("Información","Tarea agregada");
+            limpiarFormulario();
+            listarTareas();
+        }
+
+    }
+
+    private void mostrarMensaje(String titulo, String mensaje){
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.setTitle(titulo);
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
+    }
+
+    private void recolectarDatosFormulario(Tarea tarea){
+        tarea.setNombreTarea(nombreTareaTexto.getText());
+        tarea.setResponsable(responsableTexto.getText());
+        tarea.setEstatus(estatusTexto.getText());
+    }
+
+    private void limpiarFormulario(){
+        nombreTareaTexto.clear();
+        responsableTexto.clear();
+        estatusTexto.clear();
     }
 }
